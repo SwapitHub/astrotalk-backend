62db026f21df8f3cf8fab92cc0d4eadfe4c56715
shamsher singh <shamshersingh8574@gmail.com>
2025-04-16 13:25:37 +0530

    16-04

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const { razorpayRouter } = require("./routes/razorpayRouter");
 const app = express();
 const server = http.createServer(app);
 const allowedOrigins = [
-  "https://astrotalk-front-end-x3vr.vercel.app"
+  `${process.env.FRONT_END_BASE_URL}`
   // "http://localhost:3000"
 ];
 
