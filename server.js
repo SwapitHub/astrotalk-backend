5f082c046198e346fcb2104ab547f5e195bc6e70
snigdha <snig@example.com>
2025-09-01 16:11:43 +0530

    01-09-2025

--- server.js
+++ server.js
@@ -93,11 +93,11 @@ app.use(cors());
 
 // mongo db connection
 
-connectMongoDb("mongodb://localhost:27017/chatting");
+// connectMongoDb("mongodb://localhost:27017/chatting");
 
-// connectMongoDb(
-//   "mongodb+srv://swapitshamsher:Eb25QUq9aEt27aSQ@astrologer.euynurr.mongodb.net/astrotalk?retryWrites=true&w=majority&appName=astrologer"
-// );
+connectMongoDb(
+  "mongodb+srv://swapitshamsher:Eb25QUq9aEt27aSQ@astrologer.euynurr.mongodb.net/astrotalk?retryWrites=true&w=majority&appName=astrologer"
+);
 
 app.use(express.json());
 // user connect chatting socket.io
