4b596bbd60337a1d5737c437403c1acdef4041c7
snigdha <snig@example.com>
2025-09-15 12:14:57 +0530

    15-09-2025

--- controllers/saveAddressController.js
+++ controllers/saveAddressController.js
@@ -104,10 +104,31 @@ const getSaveAddress = async (req, res) => {
   }
 };
 
+const getSaveAddressId = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      return res.status(400).json({ message: "Address ID is required" });
+    }
+
+    const address = await SaveAddress.findById(id);
+
+    if (!address) {
+      return res.status(404).json({ message: "Address not found" });
+    }
+
+    res.status(200).json({ success: true, message: "Address found", data: address });
+  } catch (error) {
+    console.error("Error retrieving address:", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
 
 
 module.exports = {
   saveAddress,
   getSaveAddress,
-  updateAddress
+  updateAddress,
+  getSaveAddressId
 };
--- routes/saveAddressRoute.js
+++ routes/saveAddressRoute.js
@@ -1,12 +1,12 @@
 const express = require("express")
-const {saveAddress,getSaveAddress, updateAddress} = require("../controllers/saveAddressController")
+const {saveAddress,getSaveAddress, updateAddress, getSaveAddressId} = require("../controllers/saveAddressController")
 const addressRoute = express.Router()
 
 
 addressRoute.post("/save-address", saveAddress)
 
 addressRoute.get("/get-save-address-detail/:userMobile", getSaveAddress)
-
+addressRoute.get("/get-save-address-detail/:id", getSaveAddressId)
 addressRoute.put("/update-save-address-detail/:userMobile", updateAddress)
 
 
