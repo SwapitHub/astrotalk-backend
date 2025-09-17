87f2d85c1641c72ee9808f7f1594d4fe3f23bb1b
snigdha <snig@example.com>
2025-09-17 11:58:26 +0530

    17-09-2025

--- routes/paymentWithdrawalRouter.js
+++ routes/paymentWithdrawalRouter.js
@@ -5,8 +5,8 @@ const paymentWithdrawalRoutes = express.Router();
 
 paymentWithdrawalRoutes.post("/post-payment-withdrawal", handlePostPaymentWithdrawal)
 paymentWithdrawalRoutes.get("/get-all-payment-withdrawal", handleGetAllPaymentWithdrawal);
-paymentWithdrawalRoutes.get("/get-detail-payment-withdrawal:id", handleGetDetailPaymentWithdrawal);
-paymentWithdrawalRoutes.put("/put-payment-withdrawal:id", handlePutPaymentWithdrawal);
-paymentWithdrawalRoutes.delete("/delete-payment-withdrawal:id", handleDeletePaymentWithdrawal);
+paymentWithdrawalRoutes.get("/get-detail-payment-withdrawal/:id", handleGetDetailPaymentWithdrawal);
+paymentWithdrawalRoutes.put("/put-payment-withdrawal/:id", handlePutPaymentWithdrawal);
+paymentWithdrawalRoutes.delete("/delete-payment-withdrawal/:id", handleDeletePaymentWithdrawal);
 
 module.exports = {paymentWithdrawalRoutes}
